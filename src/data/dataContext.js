3b621a18606b7129720fe23de5b2c560f9fd210f
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-08-11 17:46:05 +0200

    create methods

--- src/data/dataContext.js
+++ src/data/dataContext.js
@@ -5,9 +5,17 @@ const DataContext = createContext();
 export const DataProvider = ({ children }) => {
   const [tickets, setTickets] = useState();
   const [destinations, setDestinations] = useState();
+  const addTicket = (ticket) => {
+    setTickets([...tickets, ticket]);
+    localStorage.setItem("tickets", JSON.stringify([...tickets, ticket]));
+  };
+  const addDestination = (destination) => {
+    setDestinations([...destinations, destination]);
+    localStorage.setItem([...destinations, destination]);
+  };
   return (
     <DataContext.Provider
-      value={{ tickets, destinations, setTickets, setDestinations }}
+      value={{ tickets, destinations, addTicket, addDestination }}
     >
       {children}
     </DataContext.Provider>
