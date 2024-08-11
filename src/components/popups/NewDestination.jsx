5159fe8796f572f0d01117237e6ee50256e66970
Salomon Tshobohwa <stshobohwa@gmail.com>
2024-08-11 15:42:12 +0200

    Finish

--- src/components/popups/NewDestination.jsx
+++ src/components/popups/NewDestination.jsx
@@ -6,7 +6,7 @@ const NewDestination = ({ close }) => {
     <div className="h-[100vh] w-[100vw] bg-blue-950/50 flex items-center justify-center z-50 fixed top-0 left-0 bottom-0 right-0">
       <div className="w-[360px] h-[400px] bg-white p-4 flex flex-col justify-between gap-4">
         <div className="w-full flex justify-between items-center">
-          <p>New Destination</p>
+          <p className="font-semibold text-blue-950">New Destination</p>
           <button className="flex items-center justify-center text-blue-950">
             <CgClose size={24} />
           </button>
